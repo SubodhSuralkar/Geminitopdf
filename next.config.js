7984423a7050fe005dfdd86e38daf06ab494ff28
ShadowX <suralkarsubodh2008@gmail.com>
2026-05-07 13:09:05 +0530

    Update next.config.js for server components
    Add webpack configuration for server externals

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+    serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium"], 
+  },
+  webpack: (config, { isserver }) => {
+    if( isserver ) {
+        config.externals.push("@sparticuz/chromium");
+    }
+      return config:
   },
 };
 
